24556c09693ae20e67f4c28ecb583f2a5118019a
andynguyenqtech <dat.bt@agiletechsoftware.com>
2023-08-04 17:13:44 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>hello world</h1>
+      <link
+        rel="apple-touch-icon"
+        href="/apple-icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+    </main>
+  );
+}
